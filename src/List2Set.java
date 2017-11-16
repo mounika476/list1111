f29a0d526e1c29ce4c339f80d72efd17b3555073
laxmireddy <laxmireddy@TRAINEE12-PC>
2017-11-16 16:09:11 +0530

    hello java

---
+++ src/List2Set.java
@@ -0,0 +1,32 @@
+
+
+import java.util.*;
+
+
+public class List2Set {
+
+	public static void main(String[] args) {
+
+
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		
+		al.add(45);
+		al.add(12);
+		al.add(45);
+		al.add(0);
+		al.add(45);
+		
+		System.out.println(al);
+		
+		for(int i:al){
+			System.out.println(i);
+		}
+		
+
+		Set<Integer> S=new HashSet<Integer>(al);
+		
+		System.out.println(S);
+		
+	}
+
+}
